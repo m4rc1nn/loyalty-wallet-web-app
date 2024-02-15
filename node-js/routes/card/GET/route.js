df620b81783eb96e519d2a33645c3c51617f8eea
MacBook (Marcin) <macbookmarcin@192.168.1.4>
2024-02-15 18:49:59 +0100

    struktura reszty routingow

--- node-js/routes/card/GET/route.js
+++ node-js/routes/card/GET/route.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const db = require('../../../database/db');
+const db = require("../../../database/db");
+const { checkToken } = require("../../../middlewares/authMiddleware");
 
-router.get("/cards", async (req, res) => {
-    const cards = await db.Card.findAll();
-    res.status(200).json({
-        status: "SUCCESS",
-        cards: cards
-    })
-})
+router.get("/cards", checkToken, async (req, res) => {
+    try {
+        const cards = await db.Card.findAll();
+        res.status(200).json({
+            status: "SUCCESS",
+            cards: cards,
+        });
+    } catch (error) {}
+});
 
 module.exports = router;
--- node-js/routes/user/GET/route.js
+++ node-js/routes/user/GET/route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const db = require("../../../database/db");
+const { checkToken } = require("../../../middlewares/authMiddleware");
 
 router.get("/users", async (req, res) => {
     try {
@@ -21,10 +22,41 @@ router.get("/users", async (req, res) => {
 router.get("/users/:id", async (req, res) => {
     const { id } = req.params;
     try {
-        const user = await db.User.findOne({ include: [{
-            model: db.Card,
-            as: 'cards'
-        }], where: { id: id } });
+        const user = await db.User.findOne({
+            include: [
+                {
+                    model: db.Card,
+                    as: "cards",
+                },
+            ],
+            where: { id: id },
+        });
+        return res.status(200).json({
+            status: "SUCCESS",
+            user: user,
+        });
+    } catch (error) {
+        return res.json(500).json({
+            type: "ERROR",
+            message: error.message,
+        });
+    }
+});
+
+router.get("/users/:id/cards", checkToken, async (req, res) => {
+    const { id } = req.params;
+    try {
+        const cards = await db.Card.findAll({
+            where: [
+                {
+                    userId: id,
+                },
+            ],
+        });
+        res.status(200).json({
+            status: "SUCCESS",
+            cards: cards,
+        });
         return res.status(200).json({
             status: "SUCCESS",
             user: user,
