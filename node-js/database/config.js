1f1953e2feabf2f8aec606784af1b1a49d01b599
MacBook (Marcin) <macbookmarcin@192.168.1.4>
2024-02-15 18:16:04 +0100

    dodanie obslugi portu do env:

--- node-js/database/config.js
+++ node-js/database/config.js
@@ -4,6 +4,7 @@ module.exports = {
     username: process.env.NODE_ENV === "development" ? "root" : process.env.DB_USER,
     password: process.env.NODE_ENV === "development" ? "" : process.env.DB_PASSWORD,
     database: process.env.NODE_ENV === "development" ? "loyalty-wallet-db" : process.env.DB_NAME,
+    port: process.env.DB_PORT,
     host: process.env.NODE_ENV === "development" ? "127.0.0.1" : process.env.DB_HOST || "127.0.0.1",
     dialect: "mysql",
 };
--- node-js/database/db.js
+++ node-js/database/db.js
@@ -6,6 +6,7 @@ const config = require("./config.js");
 const sequelize = new Sequelize(config.database, config.username, config.password, {
     host: config.host,
     dialect: "mysql",
+    port: config.port,
     logging: false,
 });
 
